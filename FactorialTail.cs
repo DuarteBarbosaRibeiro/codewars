53de3cbcad8f769f6e4a93194be282b69b352ae2
DuarteBarbosaRibeiro <99585914+DuarteBarbosaRibeiro@users.noreply.github.com>
2023-06-22 22:56:23 +0100

    FactorialTail and SoManyPermutations problems.

---
+++ FactorialTail.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+public class FactorialTail
+{
+  public static int zeroes(int radix, int number)
+  {
+    Dictionary<int, int> radix_factors = new();
+    Dictionary<int, int> number_factors = new();
+    
+    int factor = 2;
+    while (radix != 1) {
+      if (radix % factor == 0) {
+        radix_factors.TryAdd(factor, 0);
+        radix_factors[factor]++;
+        number_factors.TryAdd(factor, 0);
+        radix /= factor;
+        continue;
+      }
+      factor++;
+    }
+    
+    for (int a = 2; a <= number; a++) {
+      int a_cp = a;
+      foreach (int b in radix_factors.Keys) {
+        while (a_cp != 1 && a_cp % b == 0) {
+          number_factors[b]++;
+          a_cp /= b;
+        }
+      }
+    }
+    
+    int result = int.MaxValue;
+    
+    foreach (KeyValuePair<int, int> kvp in number_factors) {
+      int temp = kvp.Value / radix_factors[kvp.Key];
+      result = temp < result ? temp : result;
+    }
+    
+    return result;
+  }
+}
---
+++ SoManyPermutations.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+class Permutations
+{
+  /*
+    remaining: Which numbers haven't been consumed yet in the branch of the recursion.
+    sequence:  The current sequence of the permutation.
+    index:     Index in sequence where the next number should be added.
+  */
+  static IEnumerable<int[]> GetPermutationsRecursion(bool[] remaining, int[] sequence, int index) {
+    // index == remaining.Length => Permutation is completed.
+    if (index == remaining.Length)
+      yield return sequence;
+    
+    // Arrays are passed by reference, but the current
+    // array cannot be modified by the next branches,
+    // so a copy is created.
+    bool[] remaining_copy;
+    
+    // For each remaining number, do all the permutations
+    // where sequence[index] is said number.
+    for (int i = 0; i < remaining.Length; i++) {
+      if (remaining[i]) {
+        remaining_copy = new bool[remaining.Length];
+        Array.Copy(remaining, remaining_copy, remaining.Length);
+        remaining_copy[i] = false;
+        sequence[index] = i;
+        foreach (int[] result in GetPermutationsRecursion(remaining_copy, sequence, index + 1))
+          yield return result;
+      }
+    }
+  }
+  
+  // Sets up data for the recursive functions
+  static IEnumerable<int[]> GetPermutations(int n) {
+    bool[] remaining = new bool[n];
+    int[] sequence = new int[n];
+    for (int i = 0; i < n; i++)
+      remaining[i] = true;
+    foreach (int[] permutation in GetPermutationsRecursion(remaining, sequence, 0))
+      yield return permutation;
+  }
+  
+  public static List<string> SinglePermutations(string s)
+  {    
+    List<string> result = new();
+    IEnumerable<int[]> permutations = GetPermutations(s.Length);
+    foreach (int[] permutation in permutations) {
+      string str = "";
+      foreach (int index in permutation)
+        str += s[index];
+      // Since every permutation is explored
+      // there may be duplicates if there
+      // are repeating characters in s.
+      if (!result.Contains(str))
+        result.Add(str);
+    }
+    return result;
+  }
+}
